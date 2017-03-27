d9ca47ae3cd55cabbd1456c75b6989b6071262c3
unknown <shockeyt@gmail.com>
2017-03-27 14:49:09 -0600

    click removes test div

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ var test = document.createElement("div");
 test.className = "test";
 console.log(test);
 
+var celltest = document.getElementById('id1');
 function addDiv() {
-	var celltest = document.getElementById('id1');
+	// var celltest = document.getElementById('id1');
 	celltest.appendChild(test);
 	console.log(celltest);
 } 
 addDiv();
+function clickDiv() {
+	test.addEventListener("click", function() {
+		// var cellmove = document.getElementById('id6')
+		celltest.removeChild(test);
+		alert("clicked test div");
+	});
+}
+clickDiv();
