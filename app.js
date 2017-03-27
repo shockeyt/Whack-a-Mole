1309c24caef8524e0ceca696ffae2dc89f96fc11
unknown <shockeyt@gmail.com>
2017-03-27 17:16:10 -0600

    randomly generating divs w no remove

--- app.js
+++ app.js
@@ -12,26 +12,37 @@ function addDiv() {
 	console.log(celltest);
 } 
 addDiv();
+
+var go = document.querySelector('.go');
+console.log(go);
+function startGame() {
+	go.addEventListener("click", function() {
+		alert("go button pushed");
+	})
+}
+startGame();
+
 function clickDiv() {
 	test.addEventListener("click", function() {
-		
-		celltest.removeChild(test);
-		function generate() {
-			var numtest =  Math.floor((Math.random() * 25) + 1);
-			var numid = 'id' + String(numtest);
-			console.log(numtest);
-			console.log(numid);
-			
-			var table = document.getElementById(numid);
-			var newdiv = document.createElement("div");
-			newdiv.setAttribute('id', numid);
-			newdiv.className = 'test';
-			console.log(newdiv);
-			table.appendChild(newdiv);
-		}
-		generate();
+		setInterval(function(){ 
+			// celltest.removeChild(test);
+			function generate() {
+				var numtest =  Math.floor((Math.random() * 25) + 1);
+				var numid = 'id' + String(numtest);
+				console.log(numtest);
+				console.log(numid);
+				
+				var table = document.getElementById(numid);
+				var newdiv = document.createElement("div");
+				newdiv.setAttribute('id', numid);
+				newdiv.className = 'test';
+				console.log(newdiv);
+				table.appendChild(newdiv);
+			}
+			generate();
+		}, 3000);	
 		// var cellmove = document.getElementById('id6');
-		
+		celltest.removeChild(test);
 		// alert("clicked test div");
 	});
 }
