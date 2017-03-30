128004c2f2b4def73af1956b0238bc88d1ed32cd
unknown <shockeyt@gmail.com>
2017-03-29 18:21:49 -0600

    getting winner conditions

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var go = document.querySelector('.go');
 var stop = document.querySelector('.stop');
 var timediv = document.querySelector('#timer');
 var input = document.getElementById('mybox');
-
+var player1score = 0;
+var player2score = 0;
 var timeclock = 30;
 timediv.innerHTML = timeclock;
 var t;
@@ -29,15 +30,30 @@ function stopTime() {
 }
 
 function timeZero() {
-	if (timeclock === 0) {
-		alert("times up");
+	if (timeclock === 0 && currentPlayer === 1) {
+		// alert("times up");
+		console.log("round is over");
 		stopTime();
 		gametime = false;	
 		input.value = 0;
 		switchPlayer();	
+	} else if (timeclock === 0 && currentPlayer === 2) {
+		console.log(currentPlayer);
+		determineWinner();
+		stopTime();
+		gametime = false;
 	}
 }
 
+function determineWinner() {
+	if (player1score > player2score) {
+		alert("Player 1 wins!");
+	} else if (player1score < player2score) {
+		alert("Player 2 wins!");
+	} else {
+		alert("Draw!");
+	}
+}
 function switchPlayer() {
 	if (currentPlayer == 1) {
 		currentPlayer++;
@@ -102,12 +118,20 @@ function clickDiv() {
 					// alert("mole has been clicked");
 					timeStop();
 					
-    				input.value = parseInt(mybox.value, 10) + 1;
-    				console.log(currentPlayer);
+    				// input.value = parseInt(input.value, 10) + 1;
+    				
     				if (currentPlayer === 1) {
-    				player1.innerHTML = input.value;
+    					player1score++;
+	    				// player1score = input.value;
+	    				player1.textContent = player1score;
+    				console.log(player1);
     				} else if (currentPlayer === 2) {
-    				player2.innerHTML = input.value;
+    					player2score++;
+    					// player2score = input.value;
+    					player2.textContent = player2score;
+    				// player2.value = input.value;
+    				// player2.innerText = input.value;
+    				console.log(player2);
     				}
 					table.removeChild(mole);
 				})
